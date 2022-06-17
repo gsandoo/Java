8a45ef7568962db9d329e288f27ec2dee05912ee
gsandoo <rogersddt@naver.com>
2022-06-17 14:24:15 +0900

    java start

---
+++ eclipse-workspace/work/Chapter1/src/ch06/VariableTest.java
@@ -0,0 +1,18 @@
+package ch06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		byte bnum = 127;
+		// 기본적으로 정수는 int 형 4바이트로 잡힘.
+		// byte 형은 -128 ~ 127 사이의 수다
+		System.out.println(bnum);
+		
+		int number = 1000;
+		System.out.println(number);
+		
+		// int 4바이트는 -2의31승 ~ 2의31승-1이다
+		
+	}
+
+}
