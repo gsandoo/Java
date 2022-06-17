75205b592202eed72d42cb52c72cdac4b7e18f7e
gsandoo <rogersddt@naver.com>
2022-06-17 16:55:42 +0900

    Chapter 01

---
+++ eclipse-workspace/Chapter01/src/ch04/BinaryText.java
@@ -0,0 +1,17 @@
+package ch04;
+
+public class BinaryText {
+
+	public static void main(String[] args) {
+		int num = 10; // 10진수
+		int bNum = 0B1010; //2진수 *
+		int oNum = 012;	// 8진수
+		int xNum = 0XA; // 16진수
+		
+		System.out.println(num); 
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+	}
+
+}
